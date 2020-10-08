92ce63027101257de282c59f680445f5a350afa4
krzysztof <krzszk96@gmail.com>
2020-10-09 00:23:02 +0200

    portfolio glider added

--- app.js
+++ app.js
@@ -20,5 +20,47 @@ const navSlide = () => {
     //burger animation
     burger.classList.toggle("toggle");
   });
+  nav.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5 ease forwards ${
+          index / 7 + 2
+        }s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
 };
 navSlide();
+
+new Glider(document.querySelector(".glider"), {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  draggable: true,
+  dots: "#dots",
+  arrows: {
+    prev: ".glider-prev",
+    next: ".glider-next",
+  },
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
