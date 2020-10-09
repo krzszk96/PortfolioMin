5a1c7b4c2d1a33689e4c5b6ff583d6f9f60b4ba4
krzysztof <krzszk96@gmail.com>
2020-10-10 00:39:31 +0200

    responsive project gallery

--- app.js
+++ app.js
@@ -48,15 +48,21 @@ new Glider(document.querySelector(".glider"), {
   },
   responsive: [
     {
-      breakpoint: 576,
+      breakpoint: 768,
       settings: {
-        // Set to `auto` and provide item width to adjust to viewport
         slidesToShow: 2,
         slidesToScroll: 1,
       },
     },
     {
-      breakpoint: 768,
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1024,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 1,
